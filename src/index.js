fffc7866899c94d5333ac82ff958bc8293669d08
Raine Virta <raine.virta@gmail.com>
2020-07-08 13:27:23 +0300

    don't require babel-polyfill in runtime

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import $ from 'cheerio'
 import MetaTagsParser from './parsers/metatag-parser'
 import MicroRdfaParser from './parsers/micro-rdfa-parser'
 import JsonldParser from './parsers/jsonld-parser'
-if (!global._babelPolyfill) {
-  require('babel-polyfill')
-}
 
 export default function () {
   let $html = null
